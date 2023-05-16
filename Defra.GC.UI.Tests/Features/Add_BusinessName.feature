495de06d2fb8c42125c0e07c5c191ace4bed3b8d
Shiva Shankar <shiva.a.shankar@capgemini.com>
2023-05-16 17:04:31 +0100

    draft tests in progress

---
+++ Defra.GC.UI.Tests/Features/Add_BusinessName.feature
@@ -0,0 +1,16 @@
+﻿@Regression
+Feature: Business name
+
+Sign in to General Certificate Page and add businessname
+
+Scenario Outline: Navigate to General Certificate Page and Add business name
+	Given that I navigate to the NI GC application
+	Then  sign in with valid credentials with logininfo '<logininfo>'
+	When user is in Business details page
+	When user enters Business name and click on save continue
+	Then user verifys sucessfully addded business name
+
+	Examples: 
+	| logininfo |
+	| test      |
+
---
+++ Defra.GC.UI.Tests/Steps/BusinessStep.cs
@@ -0,0 +1,43 @@
+﻿using BoDi;
+using Defra.GC.UI.Tests.Tools;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using Defra.GC.UI.Tests.Pages;
+using Defra.GC.UI.Tests.Data.Users;
+
+namespace Defra.GC.UI.Tests.Steps
+{
+    [Binding]
+    public class BusinessStep
+    {
+        private readonly IObjectContainer _objectContainer;
+        private readonly ScenarioContext _scenarioContext;
+        private IWebDriver? _driver => _objectContainer.IsRegistered<IWebDriver>() ? _objectContainer.Resolve<IWebDriver>() : null;
+
+        private ISignInPage? Signin => _objectContainer.IsRegistered<ISignInPage>() ? _objectContainer.Resolve<ISignInPage>() : null;
+        private IUserObject? UserObject => _objectContainer.IsRegistered<IUserObject>() ? _objectContainer.Resolve<IUserObject>() : null;
+        private IUrlBuilder? UrlBuilder => _objectContainer.IsRegistered<IUrlBuilder>() ? _objectContainer.Resolve<IUrlBuilder>() : null;
+
+        public BusinessStep(ScenarioContext context, IObjectContainer container)
+        {
+            _scenarioContext = context;
+            _objectContainer = container;
+        }
+
+        [When(@"user is in Business details page")]
+        public void WhenUserIsInBusinessDetailsPage()
+        {
+        }
+
+        [When(@"user enters Business name and click on save continue")]
+        public void WhenUserEntersBusinessNameAndClickOnSaveContinue()
+        {
+        }
+
+        [Then(@"user verifys sucessfully addded business name")]
+        public void ThenUserVerifysSucessfullyAdddedBusinessName()
+        {
+        }
+    }
+}

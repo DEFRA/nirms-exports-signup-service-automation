6a177ba4780acd8a4604ced90f4a1867d826c1b4
Pallavi Yewale <pallavi.yewale@capgemini.com>
2023-12-20 12:00:24 +0000

    Checkbox

--- Defra.GC.UI.Tests/Pages/TermsAndConditionsPage.cs
+++ Defra.GC.UI.Tests/Pages/TermsAndConditionsPage.cs
@@ -20,7 +20,7 @@ namespace Defra.Trade.ReMos.AssuranceService.Tests.Pages
 
         private IWebElement ConfirmSanitaryAndPhytosanitaryBox => _driver.WaitForElementExists(By.Id("AssuranceCommitment"));
 
-        private IWebElement CheckBox => _driver.WaitForElement(By.XPath("//input[@id='TandCs']"));
+        private IWebElement CheckBox => _driver.WaitForElement(By.Id("TandCs"));
 
         private IWebElement ErrorMessage => _driver.WaitForElement(By.XPath("//div[contains(@class,'govuk-error-summary__body')]//a"));
 

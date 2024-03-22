b3edd24f1c1e6c4798ea6961c669a4e8587157a6
Nikita Phatak <nikita.phatak@capgemini.com>
2024-03-22 15:18:04 +0000

    Added sleep

--- Defra.GC.UI.Tests/Pages/SelfServeApplPages/SelfServeDashboardPage.cs
+++ Defra.GC.UI.Tests/Pages/SelfServeApplPages/SelfServeDashboardPage.cs
@@ -52,6 +52,7 @@ namespace Defra.Trade.ReMos.AssuranceService.Tests.Pages.SelfServeApplPages
 
         public bool VerifyBusinessRMSNumber(string businessRMSNumber) 
         {
+            Thread.Sleep(1000);
             return BusinessRMSNumberEle.Text.Contains(businessRMSNumber);
         }
 
